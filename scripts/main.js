e981a22e6747edf4bf7e11c46ef7b37cd2a67a56
Joaquin Sanchez <joaquinsanchezmiravalles@alumnos.tuniversformacion.es>
2023-12-04 10:04:33 +0100

    Creamos el objecto vehiculoObject, y lo añadimos al localStorage

--- scripts/main.js
+++ scripts/main.js
@@ -15,6 +15,19 @@ vehiculo1.arrancar();
 vehiculo1.acelerar(140);
 vehiculo1.frenar();
 
+// Creamos un objeto llamado  vehiculoObject que contenga las propiedades de vehiculo1
+const vehiculoObject = {
+    marca: "BMW",
+    modelo: "CLK",
+    color: "rojo",
+    anoFabricacion: 1992,
+    cilindrada: "2400"
+};
+
+// Guardamos el objeto VehiculoObject en el localStorage bajo la clave "vehiculo"
+
+localStorage.setItem("vehiculo", JSON.stringify(vehiculoObject));
+
 const deportivo1 = new AutomovilDeportivo("Ferrari", "Spider", "amarillo", 2020, "4000", "5000");
 mostrarResultados(`<p>Deportivo 1:</p>`)
 deportivo1.mostrarDatos();
