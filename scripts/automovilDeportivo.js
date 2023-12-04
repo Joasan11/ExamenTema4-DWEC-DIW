50f3e4e325ea5a7341acb34e87be2a433be4e416
Joaquin Sanchez <joaquinsanchezmiravalles@alumnos.tuniversformacion.es>
2023-12-04 09:11:12 +0100

    Añadimos el constructor de la clase AutomovilDeportivo

--- scripts/automovilDeportivo.js
+++ scripts/automovilDeportivo.js
@@ -2,4 +2,9 @@ import { Vehiculo } from "../scripts/vehiculo.js";
 
 export class AutomovilDeportivo extends Vehiculo {
     #potenciaMotor;
+
+    constructor(marcal, modelo, color, anoFabricacion, cilindrada){
+        super(marca, modelo, color, anoFabricacion, cilindrada);
+        this.#potenciaMotor = potenciaMotor;
+    }
 }
